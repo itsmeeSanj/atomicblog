7b57f2b874b23bd9c7d496cbf46a25cf0c662a2e
itsmeeSanj <itsmeesanj@gmail.com>
2025-01-27 07:46:17 -0500

    add:rename1

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
         setSearchQuery={setSearchQuery}
       />
       <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive ArchiveOption={ArchiveOption} />
+      <Archive ArchiveOption={ArchiveOption} onAddPost={handleAddPost} />
       <Footer />
     </section>
   );
@@ -161,7 +161,7 @@ function List({ posts }) {
   );
 }
 
-const Archive = memo(function Archive({ ArchiveOption }) {
+const Archive = memo(function Archive({ ArchiveOption, handleAddPost }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
@@ -174,7 +174,7 @@ const Archive = memo(function Archive({ ArchiveOption }) {
     <aside>
       <h2>{ArchiveOption.title}</h2>
       <button onClick={() => setShowArchive((s) => !s)}>
-        {showArchive ? "Hide archive posts" : "Show archive posts"}
+        {showArchive ? "Hide archive post" : "Show archive posts"}
       </button>
 
       {showArchive && (
@@ -184,7 +184,9 @@ const Archive = memo(function Archive({ ArchiveOption }) {
               <p>
                 <strong>{post.title}:</strong> {post.body}
               </p>
-              {/* <button onClick={() => onAddPost(post)}>Add as new post</button> */}
+              <button onClick={() => handleAddPost(post)}>
+                Add as new post
+              </button>
             </li>
           ))}
         </ul>
